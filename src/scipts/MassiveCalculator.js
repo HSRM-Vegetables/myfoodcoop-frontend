8ef29d61742ab36d516d03ad9c2eba752739aa06
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-11-23 12:43:31 +0100

    Update linter settings

--- src/scipts/MassiveCalculator.js
+++ src/scipts/MassiveCalculator.js
@@ -1,9 +1,9 @@
 export default class MassiveCalulator {
-	constructor() {
-		this.classVar = 'massive calculation that only needs javascript and is used in multiple places';
-	}
+    constructor() {
+        this.classVar = 'massive calculation that only needs javascript and is used in multiple places';
+    }
 
-	print() {
-		console.log(this.classVar, '2+2');
-	}
+    print() {
+        console.log(this.classVar, '2+2'); // eslint-disable-line
+    }
 }
--- src/stores/page.js
+++ src/stores/page.js
@@ -2,6 +2,6 @@ import { derived, readable } from 'svelte/store';
 
 export const title = readable('Stadtgemüse e.V.');
 export const titleWithSuffix = derived(
-	title,
-	($title) => `${$title} - `,
+    title,
+    ($title) => `${$title} - `,
 );
