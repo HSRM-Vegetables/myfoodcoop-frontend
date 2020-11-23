d63d8d46f86465cff78147a9ffdfabc97594bd74
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-11-23 14:22:18 +0100

    Add new Page for balance

---
+++ src/routes/balance.svelte
@@ -0,0 +1,23 @@
+<script>
+	import logo from 'images/logo.png';
+	import ShowBalance from '../components/balance/ShowBalance.svelte';
+	import { titleWithSuffix } from '../stores/page';
+</script>
+
+<style>
+	h1, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 24px;
+	}
+</style>
+
+<svelte:head>
+	<title>{$titleWithSuffix}Guthaben</title>
+</svelte:head>
+
+<h1><ShowBalance></ShowBalance></h1>
+
