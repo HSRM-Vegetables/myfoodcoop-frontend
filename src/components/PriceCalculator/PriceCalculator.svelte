2ba9c1f9c2329bd782466a0efb1e05811e6bbbbb
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-11-30 00:17:45 +0100

    [VEG-17] feat(price-calculator): Create HTML structure

---
+++ src/components/PriceCalculator/PriceCalculator.svelte
@@ -0,0 +1,61 @@
+<script>
+    // Scoped JavaScript goes here.
+</script>
+
+<style>
+</style>
+
+<div>
+    <div class="content">
+        <div class="form">
+            <h1>Preisrechner</h1>
+
+            <br />
+
+            <div class="floating">
+                <input
+                    id="input__goodsPrice"
+                    class="floating__input"
+                    name="goodsPrice"
+                    type="text"
+                    placeholder="Artikel" />
+                <label
+                    for="input__goodsPrice"
+                    class="floating__label"
+                    data-content="GoodsPrice">
+                    <span class="hidden--visually">Artikel</span>
+                </label>
+            </div>
+
+            <div class="floating">
+                <input
+                    id="input__goodsPrice"
+                    class="floating__input"
+                    name="goodsPrice"
+                    type="number"
+                    placeholder="Warenpreis" />
+                <label
+                    for="input__goodsPrice"
+                    class="floating__label"
+                    data-content="GoodsPrice">
+                    <span class="hidden--visually">Warenpreis</span>
+                </label>
+            </div>
+
+            <div class="floating">
+                <input
+                    id="input__amount"
+                    class="floating__input"
+                    name="amount"
+                    type="number"
+                    placeholder="Menge" />
+                <label
+                    for="input__amount"
+                    class="floating__label"
+                    data-content="Amount">
+                    <span class="hidden--visually">Menge</span>
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
