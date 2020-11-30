4971a09e7ded3f3a9784d75873be4345f90cdd12
Lucas Hellmann <lucashellmann@web.de>
2020-11-30 08:40:54 +0100

    add nav item, route and initial component

---
+++ src/components/shoppingCart/ShoppingCart.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from 'svelte';
+    import ShoppingCart from '../../scripts/ShoppingCart';
+
+    onMount(() => {
+
+    });
+    
+</script>
+
+
+<div>
+    <h1>Warenkorb</h1>
+
+</div>
---
+++ src/routes/shopping-cart.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ShoppingCart from '../components/shoppingCart/ShoppingCart.svelte';
+</script>
+
+
+<svelte:head>
+	<title>Warenkorb</title>
+</svelte:head>
+
+<ShoppingCart/>
