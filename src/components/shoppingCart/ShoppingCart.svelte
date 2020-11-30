da9b6a40d0426514d88688a6fa74176ee4d33958
Lucas Hellmann <lucashellmann@web.de>
2020-11-30 08:55:11 +0100

    add function to add items to cart

--- src/components/shoppingCart/ShoppingCart.svelte
+++ src/components/shoppingCart/ShoppingCart.svelte
@@ -2,9 +2,20 @@
     import { onMount } from 'svelte';
     import ShoppingCart from '../../scripts/ShoppingCart';
 
-    onMount(() => {
 
+    let cart;
+    let cartItems = [];
+
+    onMount(() => {
+        cart = new ShoppingCart();
+        cartItems = cart.items;
     });
+
+    function addSample() {
+        cart.addSampleData();
+        
+        cartItems = cart.items;
+    }    
     
 </script>
 
@@ -12,4 +23,10 @@
 <div>
     <h1>Warenkorb</h1>
 
+    <button on:click={addSample}>Sample hinzufügen</button>
+
+    <p>Artikel | Typ | Preis / Typ | Anzahl | Gesamtpreis</p>
+    {#each cartItems as item}
+        <p>{item.name} | {item.unitType} | {item.unitPrice} € | {item.quantity} | {item.unitPrice * item.quantity} € </p>
+    {/each}
 </div>
--- src/scripts/ShoppingCart.js
+++ src/scripts/ShoppingCart.js
@@ -1,3 +1,72 @@
+/*
+Local Storage Format:
+[
+    {"name":"kartoffeln", "unitType":"kg", "unitPrice":"5.00", "quantity":5}, 
+    {"name":"kürbis", "unitType":"piece", "unitPrice":"5.12", "quantity":3}
+]
+*/
+
+import LocalStorageKeys from './LocalStorageKeys';
+
 export default class ShoppingCart {
+
+    constructor() {
+        const cartString = window.localStorage.getItem(LocalStorageKeys.CART);
+        if (cartString !== null ) {
+            try {
+                this.items = JSON.parse(cartString);                
+            } catch (e) {
+                this.items = [];
+            }
+        } else {
+            this.items = [];
+        }
+    }
+
+    // to remove
+    addSampleData() {
+        this.addItem('kartoffeln', 'kg', '5', '15');
+        this.addItem('kürbis', 'piece', '5,12', '3');
+    }
+
+    addItem(name, unitType, unitPrice, quantity) {
+        if (unitType !== 'kg' && unitType !== 'piece') {
+            return false;
+        }
+
+        unitPrice = unitPrice.replace(',', '.');
+        unitPrice = parseFloat(unitPrice);
+        if(isNaN(unitPrice)) {
+            return false;
+        }            
+        unitPrice = unitPrice.toFixed(2);
+
+        quantity = parseInt(quantity);
+        
+        if(isNaN(quantity)) {
+            return false;
+        } 
+
+        // check if item name already exists in item array. if true, update entry
+        let itemExists = false;
+        for(let item of this.items) {
+            if(item.name === name) {
+                itemExists = true;
+                item['unitType'] = unitType;
+                item['unitPrice'] = unitPrice;
+                item['quantity'] = quantity;
+                break;
+            }
+        }
+
+        if(itemExists === false) {
+            this.items.push({'name':name, 'unitType':unitType, 'unitPrice':unitPrice, 'quantity':quantity})
+        }
+        
+        localStorage.setItem('sg_shoppingCart', JSON.stringify(this.items));
+    }
+
+    
+
 
 }
