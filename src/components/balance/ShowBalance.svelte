13850a8aacc1b2b031de5b76e5f8dac60c41d097
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-03 17:41:41 +0100

    [FEAT]VEG-27: Format code

--- src/components/balance/ShowBalance.svelte
+++ src/components/balance/ShowBalance.svelte
@@ -1,14 +1,13 @@
 <script>
-    import { onMount } from 'svelte';
-    import Balance from '../../scripts/Balance';
-
-    export let currentBalance = 0;
-
-    onMount(() => {
-        const balance = new Balance();
-        currentBalance = balance.money;
-    });
+   import { onMount } from 'svelte';
+   import Balance from '../../scripts/Balance';
+   
+   export let currentBalance = 0;
+   onMount(() => {
+       const balance = new Balance();
+       currentBalance = balance.money;
+   });
 </script>
 <div>
-    <span class="is-size-1">{currentBalance}€</span>
+   <span class="is-size-1">{currentBalance}€</span>
 </div>
