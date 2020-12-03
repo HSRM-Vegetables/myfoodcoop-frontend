40c183f5f4ebc8707aced2a6c8b37e45461d7885
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-03 17:41:41 +0100

    [FEAT]VEG-27: Format code

--- src/components/balance/AdjustBalance.svelte
+++ src/components/balance/AdjustBalance.svelte
@@ -1,40 +1,36 @@
 <script>
-    import { onMount } from 'svelte';
-    import Balance from '../../scripts/Balance';
-    import ShowBalance from './ShowBalance.svelte';
-    
-    let currentBalance = 0;
-    let changeMoneyInput;
-    let balance;
-    
-    onMount(() => {
-        balance = new Balance();
-        currentBalance = balance.money;
-    });
-    
-    function changeBalance() {
-        // create new value for Balance
-        balance.setBalance(changeMoneyInput.value);
-        currentBalance = balance.money;
-    }
+   import { onMount } from 'svelte';
+   import Balance from '../../scripts/Balance';
+   import ShowBalance from './ShowBalance.svelte';
+   
+   let currentBalance = 0;
+   let changeMoneyInput;
+   let balance;
+   
+   onMount(() => {
+       balance = new Balance();
+       currentBalance = balance.money;
+   });
+   
+   function changeBalance() {
+       // create new value for Balance
+       balance.setBalance(changeMoneyInput.value);
+       currentBalance = balance.money;
+   }
 </script>
-
 <style>
-    .fix-button-width{
-        width: 230px;
-    }
+   .fix-button-width{
+   width: 230px;
+   }
 </style>
-  <section class="section">
-    <div class="container has-text-centered">
-         <ShowBalance bind:currentBalance />
-
-         <input type="number" class="input mt-6" bind:this={changeMoneyInput} min="0"/>
-         
-        <a href="/balance"  type="submit" class="button is-primary fix-button-width mt-3" on:click={changeBalance}>Guthaben anpassen</a><br>
-        <a href="/balance" type="submit" class="button is-danger fix-button-width mt-3">Abbruch</a><br>
-
-    </div>
-  </section>  
+<section class="section">
+   <div class="container has-text-centered">
+      <ShowBalance bind:currentBalance />
+      <input type="number" class="input mt-6" bind:this={changeMoneyInput} min="0"/>
+      <a href="/balance"  type="submit" class="button is-primary fix-button-width mt-3" on:click={changeBalance}>Guthaben anpassen</a><br>
+      <a href="/balance" type="submit" class="button is-danger fix-button-width mt-3">Abbruch</a><br>
+   </div>
+</section>
 <div class="container has-text-centered mt-6">
-    <a href="/" type="submit" class="button is-primary">Zur Hauptseite</a><br>
+   <a href="/" type="submit" class="button is-primary">Zur Hauptseite</a><br>
 </div>
