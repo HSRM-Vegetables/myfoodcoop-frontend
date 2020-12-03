43f1aae2efbab45041d0b1a5962c1ab8014c34ec
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-03 17:00:05 +0100

    [FEAT]VEG-27: Add new style for balances

---
+++ src/routes/adjust-balance.svelte
@@ -0,0 +1,10 @@
+<script>
+	import AdjustBalance from '../components/balance/AdjustBalance.svelte';
+</script>
+
+<svelte:head>
+	<title>Guthaben anpassen</title>
+</svelte:head>
+
+<AdjustBalance/>
+
