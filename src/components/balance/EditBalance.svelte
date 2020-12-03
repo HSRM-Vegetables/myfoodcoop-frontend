3d14f4f7b640e5b31f2f8047aed9bbb20e811a19
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-03 20:36:26 +0100

    [FEAT] VEG-27: Fix button bug

--- src/components/balance/EditBalance.svelte
+++ src/components/balance/EditBalance.svelte
@@ -17,12 +17,14 @@
     
     function addToBalance() {
         if (addMoneyInput.value < 0) {
+            inputValue = null;
             error = 'Bitte geben Sie ein positven Wert ein';
         } else {
             error = '';
             balance.setBalance(balance.calcBalance(addMoneyInput.value));
             balance = balance;
-            addMoneyInput.value = '';
+            inputValue = null;
+    
         }
     }
     
@@ -37,43 +39,42 @@
     }
 </script>
 <style>
-   .fix-button-width{
+    .fix-button-width{
     width: 230px;
     }
-  .balance-input-deco{
-        position: absolute;
-        font-size: 1rem;
-        color: #ccc6c6;
-        right: 30px;
-        top: 8px;
-  }
-  .help{
+    .balance-input-deco{
+    position: absolute;
+    font-size: 1rem;
+    color: #ccc6c6;
+    right: 30px;
+    top: 8px;
+    }
+    .help{
     color: #f14668;
-  }
+    }
 </style>
 <section class="section">
-   <div class="container has-text-centered">
-      <ShowBalance bind:currentBalance="{balance.money}" />
-      <div class="columns is-centered">
-         <div class="column buttons pt-6">
-            <button class="button is-rounded" value="20" on:click="{updateInput}">20 €</button>
-            <button class="button is-rounded" value="50" on:click="{updateInput}">50 €</button>
-            <button class="button is-rounded" value="100" on:click="{updateInput}">100 €</button>
-         </div>
-      </div>
-            <div class="mt-6">
-                  <div class="has-text-left pb-2">Guthaben</div>
-                  <div class=" is-relative">
-                      <input type="number" class="input balance-input" bind:this={addMoneyInput} value="{inputValue}" placeholder="0" min="0" on:keydown="{onEnterPress}" />  
-                      <span class="balance-input-deco">€</span>
-                      <span class="help has-text-left">{error}</span>
-                  </div>
+    <div class="container has-text-centered">
+        <ShowBalance bind:currentBalance="{balance.money}" />
+        <div class="columns is-centered">
+            <div class="column buttons pt-6">
+                <button class="button is-rounded" value="20" on:click="{updateInput}">20 €</button>
+                <button class="button is-rounded" value="50" on:click="{updateInput}">50 €</button>
+                <button class="button is-rounded" value="100" on:click="{updateInput}">100 €</button>
             </div>
-
-      <button type="submit" class="button is-primary fix-button-width mt-3" on:click="{addToBalance}">Guthaben hinzufügen</button><br>
-      <a href="/adjust-balance" type="submit" class="button is-primary fix-button-width mt-3">Guthaben anpassen</a><br>
-   </div>
+        </div>
+        <div class="mt-6">
+            <div class="has-text-left pb-2">Guthaben</div>
+            <div class=" is-relative">
+                <input type="number" class="input balance-input" bind:this={addMoneyInput} value="{inputValue}" placeholder="0" min="0" on:keydown="{onEnterPress}" />  
+                <span class="balance-input-deco">€</span>
+                <span class="help has-text-left">{error}</span>
+            </div>
+        </div>
+        <button type="submit" class="button is-primary fix-button-width mt-3" on:click="{addToBalance}">Guthaben hinzufügen</button><br>
+        <a href="/adjust-balance" type="submit" class="button is-primary fix-button-width mt-3">Guthaben anpassen</a><br>
+    </div>
 </section>
 <div class="container has-text-centered mt-6">
-   <a href="/" type="submit" class="button is-primary ">Zur Hauptseite</a><br>
+    <a href="/" type="submit" class="button is-primary ">Zur Hauptseite</a><br>
 </div>
