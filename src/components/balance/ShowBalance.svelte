63956b1bb871f343fdab6ebbdbd35b68ba0dc8ac
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-03 21:50:39 +0100

    [TASK] add inline display for <showBalance />

--- src/components/balance/ShowBalance.svelte
+++ src/components/balance/ShowBalance.svelte
@@ -3,11 +3,31 @@
    import Balance from '../../scripts/Balance';
    
    export let currentBalance = 0;
+   export let type = 'big'; // inline or big
+
    onMount(() => {
        const balance = new Balance();
        currentBalance = balance.money;
    });
 </script>
+
+<style>
+   .inline-container {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+   }
+</style>
+
 <div>
-   <span class="is-size-1">{currentBalance}€</span>
+
+   {#if type === "inline"}
+      <div class="inline-container">
+         <span>Guthaben:</span>
+         <span>{currentBalance}€</span>
+      </div>
+   {:else}
+      <span class="is-size-1">{currentBalance}€</span>
+   {/if}
+
 </div>
