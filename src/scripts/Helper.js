62ea3131d7fb3511402724a5353aacacaa11a835
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-11 11:44:44 +0100

    [FEAT] veg-32: add JSDoc

--- src/scripts/Helper.js
+++ src/scripts/Helper.js
@@ -1,3 +1,9 @@
+
+/**
+ * Parses the input string as a float. Handles commas and points as denotion for decimal values
+ * @param {String} money
+ * @returns {number} The parsed value 
+ */
 export function moneyStyler(money) {
     let tmp = money;
     tmp = tmp.replace(',', '.');
