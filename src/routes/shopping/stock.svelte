eee0e61ed1c3d48fc358a30c4da5600350baa6ed
Lucas Hellmann <lucashellmann@web.de>
2020-12-14 17:31:35 +0100

    [VEG-45] feat: add route /shopping/stock

---
+++ src/routes/shopping/stock.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from 'svelte';
+	import StockList from '../../components/stock/StockList.svelte';
+	import { title } from '../../stores/page';
+	import Stock from '../../scripts/stock/Stock';
+
+	let stock = {
+        stockItems: [],
+    };
+
+    onMount(() => {
+        stock = new Stock();
+    });
+
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+$title = 'Bestand';
+</script>
+
+<svelte:head>
+	<title>Bestand</title>
+</svelte:head>
+
+<StockList bind:cartItems={stock.stockItems}/>
+
+<div class="has-text-centered">
+	<a href="/shopping/cart" class="button is-primary">Zum Warenkorb</a>
+</div>
