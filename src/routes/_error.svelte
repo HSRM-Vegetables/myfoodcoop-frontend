084a1abbb5ffc3acbfd02e1346d3dd66761e7ef2
Manuel Serret <manuel.serret@student.hs-rm.de>
2020-12-16 16:43:13 +0100

    [VEG-47] Refactor: Format file

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,33 +1,38 @@
 <script>
-import robo from 'images/robo.gif';
+    import robo from 'images/robo.gif';
 
-export let status;
-export let error;
-const dev = process.env.NODE_ENV === 'development';
+    export let status;
+    export let error;
+    const dev = process.env.NODE_ENV === 'development';
 </script>
 
 <style>
     img {
         height: 280px;
-        
     }
     h1 {
-          font-size: 2em;
-          font-weight: bold;
+        font-size: 2em;
+        font-weight: bold;
     }
     p {
-      font-size: 18px;
-      line-height: 30px;
+        font-size: 18px;
+        line-height: 30px;
     }
 </style>
+
 <div class="container has-text-centered">
-    <img src="{robo}" alt="Robo der Error Roboter" />
-  <h1>{status}: Uups, du hast das Internt kaputt gemacht</h1>
-  <p class="mt-4 mb-2">War nur ein Spaß. Aber es scheint, dass Robo, Ihr persönlicher Roboter, die Seite, nach der Sie suchen, nicht finden kann. Bitte stellen Sie sicher, dass Sie die richtige Adresse eingegeben haben, oder gehen Sie mit Hilfe des Button zurück.</p>
-  <a href="/" class="button is-primary mb-4">Zurück zur Hauptseite</a><br />
+    <img src={robo} alt="Robo der Error Roboter" />
+    <h1>{status}: Uups, du hast das Internt kaputt gemacht</h1>
+    <p class="mt-4 mb-2">
+        War nur ein Spaß. Aber es scheint, dass Robo, Ihr persönlicher Roboter,
+        die Seite, nach der Sie suchen, nicht finden kann. Bitte stellen Sie
+        sicher, dass Sie die richtige Adresse eingegeben haben, oder gehen Sie
+        mit Hilfe des Button zurück.
+    </p>
+    <a href="/" class="button is-primary mb-4">Zurück zur Hauptseite</a><br />
 </div>
 
 {#if dev && error.stack}
     <pre>{error.message}</pre>
-	<pre>{error.stack}</pre>
+    <pre>{error.stack}</pre>
 {/if}
