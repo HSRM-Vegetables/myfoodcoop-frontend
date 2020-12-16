8938946e7d12031ee4fb377574143db35abf61f7
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-16 16:21:35 +0100

    [VEG-47] style: Remove css and add a gif

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,33 @@
 <script>
-	export let status;
-	export let error;
+import robo from 'images/robo.gif';
 
-	const dev = process.env.NODE_ENV === 'development';
+export let status;
+export let error;
+const dev = process.env.NODE_ENV === 'development';
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    img {
+        height: 280px;
+        
+    }
+    h1 {
+          font-size: 2em;
+          font-weight: bold;
+    }
+    p {
+      font-size: 18px;
+      line-height: 30px;
+    }
 </style>
-
-<svelte:head>
-	<title>{status}</title>
-</svelte:head>
-
-<h1>{status}</h1>
-
-<p>{error.message}</p>
+<div class="container has-text-centered">
+    <img src="{robo}" alt="Robo der Error Roboter" />
+  <h1>{status}: Uups, du hast das Internt kaputt gemacht</h1>
+  <p class="mt-4 mb-2">War nur ein Spaß. Aber es scheint, dass Robo, Ihr persönlicher Roboter, die Seite, nach der Sie suchen, nicht finden kann. Bitte stellen Sie sicher, dass Sie die richtige Adresse eingegeben haben, oder gehen Sie mit Hilfe des Button zurück.</p>
+  <a href="/" class="button is-primary mb-4">Zurück zur Hauptseite</a><br />
+</div>
 
 {#if dev && error.stack}
+    <pre>{error.message}</pre>
 	<pre>{error.stack}</pre>
 {/if}
