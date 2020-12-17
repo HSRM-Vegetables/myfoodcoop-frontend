56f38a1e87140054b9ef8d80102dba3678aecb07
Manuel Serret <manuel.serret@student.hs-rm.de>
2020-12-17 19:40:40 +0100

    ci: introduce prettier, Marvin Kaiser

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    svelteSortOrder: "scripts-styles-markup",
+    svelteStrictMode: false,
+    svelteBracketNewLine: false,
+    svelteAllowShorthand: false,
+    plugins: ["prettier-plugin-svelte"],
+};
