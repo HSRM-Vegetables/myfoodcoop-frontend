1cca5b43cc3cf948bfdf2b5e5ba51e804d877161
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-21 18:52:07 +0100

    [VEG-93] feat: Add grid layout to StockList

--- src/components/stock/StockList.svelte
+++ src/components/stock/StockList.svelte
@@ -30,8 +30,15 @@
     .column.is-half.has-text-left {
         overflow-y: auto;
     }
-    hr {
-        margin: 0.5rem 0;
+    .shoppingElement {
+        background-color: white;
+        border-radius: 6px;
+        box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+        color: #000;
+        display: block;
+        padding: 1.25rem;
+        margin-top: 20px;
+        cursor: pointer;
     }
 </style>
 
@@ -46,29 +53,41 @@
     </div>
 
     {#each stockItems as item}
-        <hr />
-        <!-- If the component is initalized without onClick, onClick is "undefined". 
-            The expression !!undefined evaluates to false, thats why the class "is-clickable" is not applied. -->
-        <div class="columns" class:is-clickable={!!onClick} on:click={() => !!onClick && onClick(item)}>
-            {#if allowRemoval}
-                <div class="column has-text-left">
-                    <button class="button is-white" on:click={() => removeItem(item.id)}>
-                        <span class="icon">
-                            <Icon icon={mdiDelete} />
-                        </span>
-                    </button>
+        <div class="shoppingElement">
+            <!-- If the component is initalized without onClick, onClick is "undefined". 
+                The expression !!undefined evaluates to false, thats why the class "is-clickable" is not applied. -->
+            <div class="columns is-mobile" class:is-clickable={!!onClick} on:click={() => !!onClick && onClick(item)}>
+                {#if allowRemoval}
+                    <div class="column has-text-left">
+                        <button class="button is-white" on:click={() => removeItem(item.id)}>
+                            <span class="icon">
+                                <Icon icon={mdiDelete} />
+                            </span>
+                        </button>
+                    </div>
+                {/if}
+                <div class="column is-half has-text-left ">
+                    <span class="has-text-weight-bold">{item.name}</span><br />
+                    {#if item.unitType === UnitType.PIECE}
+                        <span class="is-size-7 is-hidden-desktop">{item.unitPrice} € / Stück</span>
+                    {:else}<span class="is-size-7 is-hidden-desktop">{item.unitPrice} € / kg</span>{/if}
+                    
                 </div>
-            {/if}
-            <div class="column is-half has-text-left">{item.name}</div>
-            <div class="column has-text-right">
-                {#if item.unitType === UnitType.PIECE}
-                    <span>{item.quantity} Stück</span>
-                {:else}<span>{item.quantity} kg</span>{/if}
+                <div class="column has-text-right">
+                    {#if item.unitType === UnitType.PIECE}
+                        <span>{item.quantity} Stück</span>
+                    {:else}<span>{item.quantity} kg</span>{/if}
+                </div>
+                <div class="column has-text-right is-hidden-touch">
+                    {#if item.unitType === UnitType.PIECE}
+                        <span>{item.unitPrice} € / Stück</span>
+                    {:else}<span>{item.unitPrice} € / kg</span>{/if}
+                </div>
             </div>
-            <div class="column has-text-right">
-                {#if item.unitType === UnitType.PIECE}
-                    <span>{item.unitPrice} € / Stück</span>
-                {:else}<span>{item.unitPrice} € / kg</span>{/if}
+            <div class="columns" on:click={() => !!onClick && onClick(item)}>
+                <div class="column has-text-justified">
+                    {item.description.substring(0, 200)} + '...'}
+                </div>
             </div>
         </div>
     {/each}
