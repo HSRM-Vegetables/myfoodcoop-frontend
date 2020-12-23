5d19be8781d9a51bce9873b7a24870d2068f6a82
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-23 13:01:42 +0100

    [VEG-24.12] style: Add new Nav design

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,31 +1,51 @@
 <script>
     import logo from 'images/logo_white.png';
-    import { title } from '../stores/page';
+    import { mdiBasket } from '@mdi/js';
+    import ShowBalance from './balance/ShowBalance.svelte';
+    import { name } from '../stores/user';
 </script>
 
 <style>
-    nav {
-        border-radius: 0;
-    }
-
-    .navbar-item {
-        vertical-align: middle;
-    }
-
-    .title-box {
+    .nav-element {
         color: white;
-        margin: auto;
+        background: #375a7f;
+        position: relative;
+        padding: 20px;
+    }
+    .nav-element img {
+        height: 40px;
+    }
+    .name-title {
+        position: absolute;
+        padding-left: 15px;
+        top: 23px;
+    }
+    .nav-element svg {
+        height: 40px;
+        width: auto;
+        float: right;
+        color: white;
+    }
+    a.balance {
+        padding: 50px 0;
+        color: white;
+    }
+    .blue-background {
+        height: 30px;
+        background: #375a7f;
     }
 </style>
 
-<svelte:head>
-    <title>{$title}</title>
-</svelte:head>
-
-<nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="/" tabindex="-1"> <img src={logo} alt="logo" /> </a>
-
-        <div class="title-box">{$title}</div>
-    </div>
-</nav>
+<div class="nav-element">
+    <a href="/"><img src={logo} alt="Logo" /></a>
+    <span class="is-size-4 name-title"> {$name} </span>
+    <a href="/shopping/cart">
+        <svg viewbox="0 0 24 24">
+            <path fill="currentColor" d={mdiBasket} />
+        </svg>
+    </a><br />
+    <a href="/balance" class="balance has-text-centered has-text-weight-bold">
+        <ShowBalance />
+    </a>
+</div>
+<div class="blue-background" />
