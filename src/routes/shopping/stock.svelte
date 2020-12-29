376bad6c98123eb2ee18770eac56cc2c0f647939
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2020-12-29 10:34:42 +0100

    [VEG24.12] feat: Add navbar for mobile

---
+++ src/routes/shopping/stock.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '@sapper/app';
+    import StockList from '../../components/stock/StockList.svelte';
+    import { title , navBalance } from '../../stores/page';
+    import Stock from '../../scripts/stock/Stock';
+    import { currentShoppingItem } from '../../stores/priceCalculator';
+
+    let stock = {
+        stockItems: [],
+    };
+
+    onMount(() => {
+        stock = new Stock();
+    });
+
+    /* eslint-disable prefer-const */
+    /* eslint-disable no-unused-vars */
+    $title = 'Bestand';
+    $navBalance= 'show';
+
+</script>
+
+<div class="has-text-centered mb-6">
+    <StockList
+        bind:stockItems={stock.stockItems}
+        onClick={(item) => {
+            // var will be used in another file
+            /* eslint-disable no-unused-vars */
+            $currentShoppingItem = item.name;
+            goto('/shopping/price-calculator');
+        }}
+    />
+</div>
+
+<hr />
+
+<div class="has-text-centered"><a href="/shopping/cart" class="button is-link">Zum Warenkorb</a></div>
