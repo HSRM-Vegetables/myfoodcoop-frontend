a2c5f2453cf60521e8062fc1961b9b102e3bdf81
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2020-12-30 01:30:21 +0100

    [VEG-80] fix: do not parse body if status is 204 NO CONTENT

--- src/scripts/api/Fetch.js
+++ src/scripts/api/Fetch.js
@@ -58,7 +58,10 @@ export default class Fetch {
         if (response.status >= 400) {
             throw await response.json();
         }
-        return response.json();
+        if (response.status !== 204) {
+            return response.json();
+        }
+        return undefined;
     }
     
 }
