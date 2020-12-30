24370e2c7e11ad0e5a1c6e3a3c06b3fbc1bb032c
Manuel Serret <manuel.serret@student.hs-rm.de>
2020-12-30 14:50:51 +0100

    [VEG-80] fix: add ErrorModal to stock item page

--- src/routes/stock/item/[itemId].svelte
+++ src/routes/stock/item/[itemId].svelte
@@ -1,16 +1,23 @@
 <script>
     import { stores } from '@sapper/app';
     import { onMount } from 'svelte';
+    import ErrorModal from '../../../components/common/ErrorModal.svelte';
     import StockFiller from '../../../components/stock/StockFiller.svelte';
     import Stock from '../../../scripts/stock/Stock';
 
     const { page } = stores();
     const { itemId } = $page.params;
     let item;
+    let requestError;
 
     onMount(async () => {
-        item = await Stock.getItem(itemId);
+        try {
+            item = await Stock.getItem(itemId);
+        } catch (error) {
+            requestError = error;
+        }
     });
 </script>
 
+<ErrorModal error={requestError} />
 <StockFiller item={item} />
