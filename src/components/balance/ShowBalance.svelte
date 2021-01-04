1bf410cced08e03f07eaf4c21a6d95b0474b5760
Vincent Bäertsch <vincent.baertsch@student.hs-rm.de>
2021-01-04 12:13:08 +0100

    [VEG-24.12] stlye: Add balance text to nav

--- src/components/balance/ShowBalance.svelte
+++ src/components/balance/ShowBalance.svelte
@@ -5,19 +5,9 @@
     export let type = 'big'; // inline or big
 </script>
 
-<style>
-    .inline-container {
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: space-between;
-    }
-</style>
-
 <div>
     {#if type === 'inline'}
-        <div class="inline-container">
-            <span>Guthaben:</span>
-            <span class:has-text-danger={$currentBalance < 0}>{moneyStyler($currentBalance)}€</span>
-        </div>
+        <span>Guthaben:</span>
+        <span class:has-text-danger={$currentBalance < 0}>{moneyStyler($currentBalance)}€</span>
     {:else}<span class:has-text-danger={$currentBalance < 0}>{moneyStyler($currentBalance)}€</span>{/if}
 </div>
