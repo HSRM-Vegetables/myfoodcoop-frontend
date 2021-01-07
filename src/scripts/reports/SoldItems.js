397a91186f14e13ee170c7d0e4f39f97b3e7c40b
Lucas Hellmann <lucashellmann@web.de>
2021-01-07 08:21:14 +0100

    [VEG-116] feat: use api

--- src/scripts/reports/SoldItems.js
+++ src/scripts/reports/SoldItems.js
@@ -1,27 +1,9 @@
+import Fetch from "../api/Fetch";
+
 export default class SoldItems {
 
     static async getItems(fromDate, toDate) {
-        // const response = await Fetch.get('/reports/sold-items');
-        const resonse = {
-            "items": [
-                {
-                    "id": "0794e2e2-f5b2-42fb-a2b3-e12c75897f85",
-                    "name": "Kartoffeln",
-                    "fromDate": fromDate,
-                    "toDate": toDate,
-                    "quantitySold": 40.2,
-                    "unitType": "WEIGHT"
-                },
-                {
-                    "id": "1234e2e2-f5b2-42fb-a2b3-e12c75897123",
-                    "name": "Kürbis",
-                    "fromDate": "2017-07-21",
-                    "toDate": "2017-07-21",
-                    "quantitySold": 80,
-                    "unitType": "PIECE"
-                }
-            ]
-        }
-        return resonse.items;
+        const response = await Fetch.get(`reports/sold-items?fromDate=${ fromDate }&toDate=${ toDate}`);
+        return response.items;
     }
 }
