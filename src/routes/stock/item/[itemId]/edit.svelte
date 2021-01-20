227389b034b3923aa7f6d60956252a9cfadfb7cd
Lucas Hellmann <lucashellmann@web.de>
2021-01-20 19:29:30 +0100

    [VEG-54] feat: new route for item edit

--- src/routes/stock/item/[itemId]/edit.svelte
+++ src/routes/stock/item/[itemId]/edit.svelte
@@ -1,10 +1,10 @@
 <script>
     import { stores } from '@sapper/app';
     import { onMount } from 'svelte';
-    import ErrorModal from '../../../components/common/ErrorModal.svelte';
-    import StockFiller from '../../../components/stock/StockFiller.svelte';
-    import Stock from '../../../scripts/stock/Stock';
-    import { title, navBalance } from '../../../stores/page';
+    import ErrorModal from '../../../../components/common/ErrorModal.svelte';
+    import StockFiller from '../../../../components/stock/StockFiller.svelte';
+    import Stock from '../../../../scripts/stock/Stock';
+    import { title, navBalance } from '../../../../stores/page';
 
     /* eslint-disable prefer-const */
     /* eslint-disable no-unused-vars */
