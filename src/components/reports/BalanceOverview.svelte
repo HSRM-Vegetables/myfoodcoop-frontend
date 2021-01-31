65b3f74ad7b95f5a9e6d8896227aea986bddb87a
Lucas Hellmann <lucashellmann@web.de>
2021-01-31 15:32:56 +0100

    [VEG-236] feat: add component

---
+++ src/components/reports/BalanceOverview.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    /**
+     * A list of users and their balances
+     */
+    export let userBalanceList;
+
+    const selectEvent = createEventDispatcher();
+</script>
+
+{#if userBalanceList}
+    <div class="columns is-mobile">
+        <div class="column has-text-weight-bold">Benutzername</div>
+        <div class="column has-text-weight-bold has-text-right">aktuelles Guthaben</div>
+    </div>
+    {#each userBalanceList.sort((a, b) => a.balance - b.balance) as user}
+        <div class="columns is-mobile is-clickable" on:click={selectEvent('select', { id: user.id })}>
+            <div class="column is-three-quarters item_name">{user.username}</div>
+            <div class="column has-text-right">{user.balance}</div>
+        </div>
+    {/each}
+{/if}
--- src/routes/reports/balance-overview/index.svelte
+++ src/routes/reports/balance-overview/index.svelte
@@ -1,9 +1,11 @@
 <script>
     import { mdiArrowLeft } from '@mdi/js';
+    import { goto } from '@sapper/app';
     import { title, navBalance } from '../../../stores/page';
     import Loader from '../../../components/common/Loader.svelte';
     import ErrorModal from '../../../components/common/ErrorModal.svelte';
     import AuthorizeByRoles, { Roles } from '../../../components/common/AuthorizeByRoles.svelte';
+    import BalanceOverviewComp from '../../../components/reports/BalanceOverview.svelte';
     import BalanceOverview from '../../../scripts/reports/BalanceOverview';
     import Button from '../../../components/common/Button.svelte';
 
@@ -31,6 +33,10 @@
         }
     }
 
+    function userSelected(event) {
+        goto(`users/${event.detail.id}`);
+    }
+
     loadBalances();
 </script>
 
@@ -43,7 +49,7 @@
             <div class="message-body">Leider ist beim Abrufen der Daten etwas schief gelaufen.</div>
         </article>
     {:else if userBalanceList}
-        <!-- TODO: Component -->
+        <BalanceOverviewComp userBalanceList={userBalanceList} on:select={userSelected} />
     {/if}
 
     <ErrorModal error={requestError} />
