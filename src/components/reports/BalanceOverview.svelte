3509f49b80f02c3ceb5a10daf681fa21e643d533
Lucas Hellmann <lucashellmann@web.de>
2021-02-03 17:28:09 +0100

    [VEG-236] refactor: make prettier happy

--- src/components/reports/BalanceOverview.svelte
+++ src/components/reports/BalanceOverview.svelte
@@ -17,7 +17,11 @@
         <div class="column has-text-weight-bold has-text-right">aktuelles Guthaben</div>
     </div>
     {#each userBalanceList.sort((a, b) => a.balance - b.balance) as user}
-        <ListItem isClickable={true} highlight={user.balance < 0} on:click={() => selectEvent('select', { id: user.id })}>
+        <ListItem
+            isClickable={true}
+            highlight={user.balance < 0}
+            on:click={() => selectEvent('select', { id: user.id })}
+        >
             <div class="columns is-mobile is-clickable">
                 <div class="column is-three-quarters item_name">{user.username}</div>
                 <div class="column has-text-right">{moneyStyler(user.balance)} €</div>
