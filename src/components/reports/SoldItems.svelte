b32f58413e9fb98b5edd2a4fabdab3e297d3b047
Tatjana Merkel <tatjana.merkel@student.hs-rm.de>
2021-02-04 17:29:07 +0100

    Feat/veg 120 gross net summary

--- src/components/reports/SoldItems.svelte
+++ src/components/reports/SoldItems.svelte
@@ -11,23 +11,29 @@
 </script>
 
 <style>
-    .item_name {
+    .item-name {
         word-break: break-all;
     }
 </style>
 
 {#if soldItems !== undefined}
     <div class="columns is-mobile">
-        <div class="column has-text-weight-bold">Artikelname</div>
-        <div class="column has-text-weight-bold has-text-right">verkaufte Menge</div>
+        <div class="column has-text-weight-bold has-text-left">Artikelname</div>
+        <div class="column has-text-weight-bold has-text-centered">verkaufte Menge</div>
+        <div class="column has-text-weight-bold has-text-centered is-hidden-touch">MwSt. Satz</div>
+        <div class="column has-text-weight-bold has-text-centered is-hidden-touch">Steuern</div>
+        <div class="column has-text-weight-bold has-text-right">Brutto</div>
     </div>
     {#each soldItems.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)) as item}
         <div class="columns is-mobile is-clickable" on:click={selectEvent('select', { id: item.id })}>
-            <div class="column is-three-quarters item_name">{item.name}</div>
-            <div class="column has-text-right">
+            <div class="column item-name has-text-left">{item.name}</div>
+            <div class="column has-text-centered">
                 {item.quantitySold}
                 {item.unitType === UnitType.PIECE ? 'Stück' : 'kg'}
             </div>
+            <div class="column has-text-centered is-hidden-touch">{item.vat}%</div>
+            <div class="column has-text-centered is-hidden-touch">{item.totalVat}€</div>
+            <div class="column has-text-right">{item.grossAmount}€</div>
         </div>
     {/each}
 {/if}
