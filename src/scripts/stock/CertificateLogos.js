63e6d6a6dcddfcdec5f0efabad2b37464bbf7c6d
Marvin Kaiser <marvin.kaiser.93@gmail.com>
2021-02-10 15:20:31 +0100

    feat: add mini cert icons to improve loading times

--- src/scripts/stock/CertificateLogos.js
+++ src/scripts/stock/CertificateLogos.js
@@ -3,21 +3,30 @@ import Biosiegel from 'images/certificate/biosiegel.png';
 import Demeter from 'images/certificate/demeter.png';
 import Naturland from 'images/certificate/naturland.png';
 
+import BiolandMini from 'images/certificate/bioland_mini.png';
+import BiosiegelMini from 'images/certificate/biosiegel_mini.png';
+import DemeterMini from 'images/certificate/demeter_mini.png';
+import NaturlandMini from 'images/certificate/naturland_mini.png';
+
 export const CertificateLogos = [
     {
         name: 'Bioland',
         image: Bioland,
+        imageMini: BiolandMini,
     },
     {
         name: 'Biosiegel',
         image: Biosiegel,
+        imageMini: BiosiegelMini,
     },
     {
         name: 'Demeter',
         image: Demeter,
+        imageMini: DemeterMini,
     },
     {
         name: 'Naturland',
         image: Naturland,
+        imageMini: NaturlandMini,
     }
 ];
